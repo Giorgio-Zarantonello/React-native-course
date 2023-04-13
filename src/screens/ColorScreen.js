77699906737a14d925edc917c5222eeafc239320
Giorgio-Zarantonello <giorgio.zarantonello@gmail.com>
2023-04-14 01:41:01 +0200

    .

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -3,25 +3,46 @@ import { View, StyleSheet, Button, FlatList } from 'react-native';
 
 const ColorScreen = () => {
   const [colors, setColors] = useState([]);
-  console.log(colors);
+  const [count, setCount] = useState(0);
+
+  const handleAddColor = () => {
+    if (count < 50) {
+      if (count % 10 === 0) {
+        setColors([...colors, [randomRgb()]]);
+        setCount(count + 1);
+      } else {
+        setColors([
+          ...colors.slice(0, -1),
+          [...colors[colors.length - 1], randomRgb()],
+        ]);
+        setCount(count + 1);
+      }
+    }
+    else {
+      null;
+    }
+  };
 
   return (
     <View>
-      <Button
-        title="Add a Color"
-        onPress={() => {
-          setColors([...colors, randomRgb()]);
-        }}
-      />
+      <Button title="Add a Color" onPress={handleAddColor} />
 
       <FlatList
-        keyExtractor={item => item}
-        data={colors}
-        renderItem={({ item }) => {
-          return (
-            <View style={{ height: 50, width: 50, backgroundColor: item }} />
-          );
+        style={styles.list}
+        contentContainerStyle={{
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'flex-start',
         }}
+        keyExtractor={(item, index) => index.toString()}
+        data={colors}
+        renderItem={({ item }) => (
+          <View style={styles.colorColumn}>
+            {item.map((color, index) => (
+              <View key={index} style={styles.colorBox(color)} />
+            ))}
+          </View>
+        )}
       />
     </View>
   );
@@ -35,6 +56,20 @@ const randomRgb = () => {
   return `rgb(${red}, ${green}, ${blue})`;
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  list: {
+    margin: 45,
+  },
+  colorColumn: {
+    marginRight: 10,
+  },
+  colorBox: color => ({
+    height: 50,
+    width: 50,
+    backgroundColor: color,
+    marginBottom: 10,
+
+  }),
+});
 
 export default ColorScreen;
