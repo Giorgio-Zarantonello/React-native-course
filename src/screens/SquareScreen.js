e38d807a3978c9768a1e9880b8c32e7f3c436a25
Giorgio-Zarantonello <giorgio.zarantonello@gmail.com>
2023-04-13 16:33:05 +0200

    added Validation

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -16,11 +16,12 @@ const funReducer = (state, action) => {
         case 'red':
             // never do something like : state.red = state.red - 15 ;
             //instead , rebuild the state overwrited with the updated data 
-            return { ...state, red: state.red + action.amount };
+            return state.red + action.amount > 255 || state.red + action.amount < 0 ? state : { ...state, red: state.red + action.amount };
+            
         case 'green':
-            return { ...state, green: state.green + action.amount };
+            return state.green + action.amount > 255 || state.green + action.amount < 0 ? state : { ...state, green: state.green + action.amount };
         case 'blue':
-            return { ...state, blue: state.blue + action.amount };
+            return state.blue + action.amount > 255 || state.blue + action.amount < 0 ? state : { ...state, blue: state.blue + action.amount };
         default:
             return state;
     }
